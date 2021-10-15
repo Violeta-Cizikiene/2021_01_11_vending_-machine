a998c3fe9ff25d23b96b8375b6e48645b7552fe3
violeta <v.aksamitauskaite@gmail.com>
2021-10-15 23:23:09 +0300

    responsive

--- index.php
+++ index.php
@@ -34,86 +34,88 @@ $vendingMachine = new VendingMachine($items);
 <!-- 14 _ produktai atvaizduoti lenteleje
  -->
  <body>
+
+	<div class="container">
+		<h1>VENDING MACHINE</h1>
 	
-	<h1>VENDING MACHINE</h1>
-
-	<table>
-		<tr>
-			<th class="informaton">
-				Name
-			</th>
-
-			<th class="informaton">
-				Code
-			</th>
-
-			<th class="informaton">
-				Quantity
-			</th>
-
-			<th class="informaton">
-				Price Eur
-			</th>
-		</tr>
-
-		<!-- 15 _ kuriamas ciklas, kuris atvaizduoja produktus lenteleje -->
-		<?php
-			foreach ($items as $key => $item):
-		?>
-			<!-- 17 _ kuriamas html lenteles vienas row -->
+		<table>
 			<tr>
-				<th>
-					<?php echo $item->getName()?>
+				<th class="informaton">
+					Name
 				</th>
 
-				<th>
-					<?php echo $item->getCode()?>
+				<th class="informaton">
+					Code
 				</th>
 
-				<th>
-					<?php echo $item->getQuantity()?>
+				<th class="informaton">
+					Quantity
 				</th>
 
-				<th>
-					<?php echo $item->getPrice()?>
+				<th class="informaton">
+					Price Eur
 				</th>
 			</tr>
 
-		<!-- 16 _ baigiamas foreach ciklas -->
+			<!-- 15 _ kuriamas ciklas, kuris atvaizduoja produktus lenteleje -->
+			<?php
+				foreach ($items as $key => $item):
+			?>
+				<!-- 17 _ kuriamas html lenteles vienas row -->
+				<tr>
+					<th>
+						<?php echo $item->getName()?>
+					</th>
+
+					<th>
+						<?php echo $item->getCode()?>
+					</th>
+
+					<th>
+						<?php echo $item->getQuantity()?>
+					</th>
+
+					<th>
+						<?php echo $item->getPrice()?>
+					</th>
+				</tr>
+
+			<!-- 16 _ baigiamas foreach ciklas -->
+			<?php
+				endforeach;
+			?>
+		</table>
+
+		<!-- 25 _ kuriama forma
+		autocomplete="off" isjungia pries tai ivestus pasiulymus -->
+		<form action="" method="post" autocomplete="off">
+
+			<div class="input-group">
+				<input placeholder="enter product code" class="" type="text" name="code" id="code" value="">
+			</div>
+
+			<div class="input-group">
+				<input placeholder="enter amount of money" class="" type="text" name="money" id="money" value="">
+			</div>
+
+			<button type='submit'>Buy Snack</button>
+
+		</form>
+		
+		<!-- 26 _ vykdomas metodas post paspaudus submit _ narsykle perkraus nebe su get, o su post -->
 		<?php
-			endforeach;
+			if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+				// 28 _ kvieciamas vend metodas
+				// $_POST - specialus php kintamasis,
+				// kuris yra masyvo tipo ir laiko visus
+				// is formos atsiustus duomenis
+				// 30 _ irasomas papildomas raktas money (atsiusta reiksme is inputo)
+				// 36 _ php ["money"] paverciama skaiciais naudojant floatval _ $moneySum(vadinasi vend metode)
+				// (int) naudojamas, tik tuomet, kai norima gauti sveikuosius skaicius
+				echo $vendingMachine->vend($_POST["code"], floatval($_POST["money"]));
+			}
 		?>
-	</table>
-
-	<!-- 25 _ kuriama forma
-	autocomplete="off" isjungia pries tai ivestus pasiulymus -->
-	<form action="" method="post" autocomplete="off">
-
-		<div class="input-group">
-			<input placeholder="enter product code" class="" type="text" name="code" id="code" value="">
-		</div>
-
-		<div class="input-group">
-			<input placeholder="enter amount of money" class="" type="text" name="money" id="money" value="">
-		</div>
-
-		<button type='submit'>Buy Snack</button>
-
-	</form>
-
-	<!-- 26 _ vykdomas metodas post paspaudus submit _ narsykle perkraus nebe su get, o su post -->
-	<?php
-		if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-			// 28 _ kvieciamas vend metodas
-			// $_POST - specialus php kintamasis,
-			// kuris yra masyvo tipo ir laiko visus
-			// is formos atsiustus duomenis
-			// 30 _ irasomas papildomas raktas money (atsiusta reiksme is inputo)
-			// 36 _ php ["money"] paverciama skaiciais naudojant floatval _ $moneySum(vadinasi vend metode)
-			// (int) naudojamas, tik tuomet, kai norima gauti sveikuosius skaicius
-			echo $vendingMachine->vend($_POST["code"], floatval($_POST["money"]));
-		}
-	?>
+	</div>
 </body>
 
 </html>
